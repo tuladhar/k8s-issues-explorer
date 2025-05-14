8e0423dbaf6a522ab1bf5e56f22e740f8f4f7952
Puru <5674762+tuladhar@users.noreply.github.com>
2025-05-14 14:51:50 +0545

    fix: fill

--- components/icons.tsx
+++ components/icons.tsx
@@ -45,7 +45,7 @@ export function KubernetesLogo(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg {...props} version="1.0" xmlns="http://www.w3.org/2000/svg"
     width="1024.000000pt" height="1024.000000pt" viewBox="0 0 1024.000000 1024.000000"
-    preserveAspectRatio="xMidYMid meet">
+    preserveAspectRatio="xMidYMid meet" fill="currentColor">
 
     <g transform="translate(0.000000,1024.000000) scale(0.100000,-0.100000)"
     fill="#000000" stroke="none">
